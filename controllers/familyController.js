d654b430ac198977e619170ba5db30439fb25afe
benpemberton <benedictpemberton@gmail.com>
2023-07-17 09:34:53 +0000

    adding family create form

--- controllers/familyController.js
+++ controllers/familyController.js
@@ -11,58 +11,50 @@ exports.list = asyncHandler(async (req, res, next) => {
   });
 });
 
-// Display detail page for a specific Author.
-exports.author_detail = asyncHandler(async (req, res, next) => {
-  // Get details of author and all their books (in parallel)
-  const [author, allBooksByAuthor] = await Promise.all([
-    Author.findById(req.params.id).exec(),
-    Book.find({ author: req.params.id }, "title summary").exec(),
+// Display detail page for a specific Family.
+exports.detail = asyncHandler(async (req, res, next) => {
+  // Get details of family and all their instruments (in parallel)
+  const [family, instrumentsInFamily] = await Promise.all([
+    Family.findById(req.params.id).exec(),
+    Instrument.find({ family: req.params.id }, "name description").exec(),
   ]);
 
-  if (author === null) {
+  if (family === null) {
     // No results.
-    const err = new Error("Author not found");
+    const err = new Error("Family not found");
     err.status = 404;
     return next(err);
   }
 
-  res.render("author_detail", {
-    title: "Author Detail",
-    author: author,
-    author_books: allBooksByAuthor,
+  res.render("family/detail", {
+    title: "Family Detail",
+    family: family,
+    instruments: instrumentsInFamily,
   });
 });
 
-// Display Author create form on GET.
-exports.author_create_get = (req, res, next) => {
-  res.render("author_form", { title: "Create Author" });
+// Display Family create form on GET.
+exports.create_get = (req, res, next) => {
+  res.render("family/form", { title: "Create Family" });
 };
 
-// Handle Author create on POST.
-exports.author_create_post = [
+// Handle Family create form on POST.
+exports.create_post = [
   // Validate and sanitize fields.
-  body("first_name")
+  body("name")
     .trim()
-    .isLength({ min: 1 })
+    .isLength({ min: 3, max: 100 })
     .escape()
-    .withMessage("First name must be specified.")
+    .withMessage("Name must be specified.")
     .isAlphanumeric()
-    .withMessage("First name has non-alphanumeric characters."),
-  body("family_name")
+    .withMessage("Name has non-alphanumeric characters."),
+  body("description")
     .trim()
-    .isLength({ min: 1 })
+    .isLength({ min: 3, max: 200 })
     .escape()
-    .withMessage("Family name must be specified.")
+    .withMessage("Description must be specified.")
     .isAlphanumeric()
-    .withMessage("Family name has non-alphanumeric characters."),
-  body("date_of_birth", "Invalid date of birth")
-    .optional({ values: "falsy" })
-    .isISO8601()
-    .toDate(),
-  body("date_of_death", "Invalid date of death")
-    .optional({ values: "falsy" })
-    .isISO8601()
-    .toDate(),
+    .withMessage("Description has non-alphanumeric characters."),
 
   // Process request after validation and sanitization.
   asyncHandler(async (req, res, next) => {
@@ -70,28 +62,26 @@ exports.author_create_post = [
     const errors = validationResult(req);
 
     // Create Author object with escaped and trimmed data
-    const author = new Author({
-      first_name: req.body.first_name,
-      family_name: req.body.family_name,
-      date_of_birth: req.body.date_of_birth,
-      date_of_death: req.body.date_of_death,
+    const family = new Family({
+      name: req.body.name,
+      description: req.body.description,
     });
 
     if (!errors.isEmpty()) {
       // There are errors. Render form again with sanitized values/errors messages.
-      res.render("author_form", {
-        title: "Create Author",
-        author: author,
+      res.render("family/form", {
+        title: "Create Family",
+        family: family,
         errors: errors.array(),
       });
       return;
     } else {
       // Data from form is valid.
 
-      // Save author.
-      await author.save();
+      // Save family.
+      await family.save();
       // Redirect to new author record.
-      res.redirect(author.url);
+      res.redirect(family.url);
     }
   }),
 ];
--- routes/familyRouter.js
+++ routes/familyRouter.js
@@ -2,16 +2,14 @@ let express = require("express");
 let router = express.Router();
 const family_controller = require("../controllers/familyController");
 
-/// FAMILY ROUTES ///
-
 // GET list of instrument families
 router.get("/", family_controller.list);
 
-// // GET request for creating a Family. NOTE This must come before routes that display Family (uses id).
-// router.get("/create", family_controller.family_create_get);
+// GET request for creating a Family. NOTE This must come before routes that display Family (uses id).
+router.get("/create", family_controller.create_get);
 
-// // POST request for creating Family.
-// router.post("/create", family_controller.family_create_post);
+// POST request for creating Family.
+router.post("/create", family_controller.create_post);
 
 // // GET request to delete Family.
 // router.get("/:id/delete", family_controller.family_delete_get);
@@ -25,7 +23,7 @@ router.get("/", family_controller.list);
 // // POST request to update Family.
 // router.post("/:id/update", family_controller.family_update_post);
 
-// // GET request for one Family.
-// router.get("/:id", family_controller.family_detail);
+// GET request for one Family.
+router.get("/:id", family_controller.detail);
 
 module.exports = router;

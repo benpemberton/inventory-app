05bf95b8605f94b8b7e094d5bdaa07c5d0a58926
Ben <benedictpemberton@gmail.com>
2023-08-04 09:57:12 +0100

    changed mongodb credentials

--- app.js
+++ app.js
@@ -22,8 +22,7 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 
 // Define the database URL to connect to.
-const mongoDB =
-  "mongodb+srv://dbadmin:npGfy1oEwwZdbDXj@cluster0.ovy6kzy.mongodb.net/inventory_app?retryWrites=true&w=majority";
+const mongoDB = process.env.MONGO_URI;
 
 // Wait for database to connect, logging an error if there is a problem
 main().catch((err) => console.log(err));
